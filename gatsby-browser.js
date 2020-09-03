8866de1c227e067d424f735bcc6d96e0a21c1dd8
Jiří Pudil <me@jiripudil.cz>
2020-09-03 10:41:44 +0200

    fix the huge coffee icon flash on initial page load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,6 @@
 import 'normalize.css';
 import './src/styles/prism-theme.css';
+
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import {config} from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
