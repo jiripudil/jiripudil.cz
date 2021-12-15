3416941df27b7769ad250a62a682602cf85de5cf
Jiří Pudil <me@jiripudil.cz>
2021-12-15 10:08:01 +0100

    chore: replace GA with Insights

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,7 @@ import './src/styles/prism-theme.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import {config} from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
+
+import insights from 'insights-js';
+insights.init('L5zYe2dksvRS7mB7');
+insights.trackPages();
