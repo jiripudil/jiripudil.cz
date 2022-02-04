28f5cf84fd5040cc93c7a9182d8c4a405e6726b8
Jiří Pudil <me@jiripudil.cz>
2022-02-04 12:53:40 +0100

    chore: remove analytics altogether, I don't look at it anyway

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,5 @@
 import 'normalize.css';
 import './src/styles/prism-theme.css';
-import * as insights from 'insights-js';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import {config} from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
-
-insights.init('L5zYe2dksvRS7mB7');
-insights.trackPages();
